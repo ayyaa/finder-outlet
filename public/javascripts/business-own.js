b57cf1f05c2bee82e1dfe90c3d622776e13b56a0
ayyaa <ayyaa.ats12@gmail.com>
2018-05-14 09:53:08 +0700

    revisi

--- public/javascripts/business-own.js
+++ public/javascripts/business-own.js
@@ -43,3 +43,18 @@
     event.preventDefault();
   });
 })(jQuery); // End of use strict
+
+// Get the container element
+var btnContainer = document.getElementById("exampleAccordion");
+
+// Get all buttons with class="btn" inside the container
+var btns = btnContainer.getElementsByClassName("nav-item");
+
+// Loop through the buttons and add the active class to the current/clicked button
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    var current = document.getElementsByClassName("active");
+    current[0].className = current[0].className.replace(" active", "");
+    this.className += " active";
+  });
+}
