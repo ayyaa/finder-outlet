05f3813c161817817e467a7c175efffcba91326b
ayyaa <ayyaa.ats12@gmail.com>
2018-05-15 16:45:43 +0700

    add active side bar

--- routes/admin.js
+++ routes/admin.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/dashboard', function(req, res, next) {
-  res.render('admin/dashboard', { title: 'Express' });
+  res.render('admin/dashboard', { title: 'Express' , active1: 'active'});
 });
 
 
@@ -20,31 +20,35 @@ router.get('/edit-category', function(req, res, next) {
 });
 
 router.get('/list-business', function(req, res, next) {
-  res.render('admin/list-business');
+  res.render('admin/list-business', {  active3: 'active'});
 });
 
 router.get('/list-outlets', function(req, res, next) {
-  res.render('admin/list-outlets');
+  res.render('admin/list-outlets', {  active5: 'active'});
 });
 
 router.get('/list-categories', function(req, res, next) {
-  res.render('admin/list-categories');
+  res.render('admin/list-categories', {  active2: 'active'});
 });
 
 router.get('/list-reviews', function(req, res, next) {
-  res.render('admin/list-reviews');
+  res.render('admin/list-reviews', {  active6: 'active'});
 });
 
 router.get('/list-report-reviews', function(req, res, next) {
-  res.render('admin/list-report-reviews');
+  res.render('admin/list-report-reviews', {  active7: 'active'});
 });
 
 router.get('/list-business-owner', function(req, res, next) {
-  res.render('admin/list-bo');
+  res.render('admin/list-bo', {  active4: 'active'});
 });
 
 router.get('/lay', function(req, res, next) {
   res.render('admin/layout-admin');
 });
+
+// router.get('/create-category', function(req, res, next) {
+//   res.render('admin/layout-admin');
+// });
 
 module.exports = router;
